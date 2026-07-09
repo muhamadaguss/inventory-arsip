65d5f90d6bd9f8be31a8cbe921e3c858518d8f6c
muhamadaguss <muhamadagus3197@gmail.com>
2026-07-09 10:59:44 +0700

    chore: remove disposable demo module now that real endpoints are coming

--- court-archive-backend/src/demo/demo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DemoController } from './demo.controller';
-import { AuthModule } from '../auth/auth.module';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [DemoController],
-})
-export class DemoModule {}
